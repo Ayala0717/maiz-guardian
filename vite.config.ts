a08eb13f71e3016dc6e68c08e94271c6f7357739
Roberto Ayala <rbt.ayala20@gmail.com>
2025-09-22 09:53:50 -0600

    feat - agregado auto identificador de componentes

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Components from 'unplugin-vue-components/vite'
 import Pages from 'vite-plugin-pages'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), Pages()],
+  plugins: [
+    Components({
+      dirs: ['src/components', 'src/layouts'],
+      dts: 'src/components.d.ts',
+      extensions: ['vue', 'svg'],
+      directoryAsNamespace: true,
+      importPathTransform: (path: string) =>
+        path.endsWith('.svg') ? `${path}?component` : undefined,
+    }),
+    vue(),
+    Pages(),
+  ],
 })
