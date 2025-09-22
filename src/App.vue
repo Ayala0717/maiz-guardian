95792005d2a5e753ad95a99741ec3641fec7c38f
Roberto Ayala <rbt.ayala20@gmail.com>
2025-09-22 11:22:53 -0600

    feat - agregado meta data

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,10 @@
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld :msg="t('hello')" />
+  <RouterView />
 </template>
 
 <script lang="ts">
+import { useHead } from '@unhead/vue'
 import { useI18n } from 'vue-i18n'
-
 export default {
   name: 'App',
 }
@@ -20,19 +12,17 @@ export default {
 
 <script lang="ts" setup>
 const { t } = useI18n()
-</script>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+useHead({
+  title: t('titleApp'),
+  meta: [
+    {
+      name: 'description',
+      content: t('descriptionApp'),
+    },
+  ],
+  htmlAttrs: {
+    lang: 'es',
+  },
+})
+</script>
